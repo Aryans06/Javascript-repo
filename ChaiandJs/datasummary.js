e2cb10543cfec135da69774f0359395034abe2b5
Aryan Jha <aryans2637@gmail.com>
2024-12-31 11:56:46 +0530

    Summar of data types

---
+++ ChaiandJs/datasummary.js
@@ -0,0 +1,15 @@
+// Primitve
+// 7types: string,Number,Bigint,symbol,null,boolean,undefined
+
+//reference(no primitive)
+
+//Array,objects,functions
+
+ let obj={
+  name:"Aryan", 
+  age:22
+}
+
+const myFunction= function(){
+  console.log("Hello")
+}
