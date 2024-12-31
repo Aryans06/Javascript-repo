b05395fa07fe3607f9773ebc352d270c2351a2a9
Aryan Jha <aryans2637@gmail.com>
2024-12-31 10:39:12 +0530

    "Added"

--- ChaiandJs/DatatypeConversion.js
+++ ChaiandJs/DatatypeConversion.js
@@ -1,8 +1,18 @@
-let score=null
+/*let score=null
 console.log( typeof score)
 
 let value=Number(score)
 console.log(typeof value)
-console.log(value)
+console.log(value)*/
 
 // 33-number, 33abc-Nan , null-0
+
+let a="Aryan"
+let b="Jha"
+c=a+b
+console.log(c)
+
+console.log(1+4)
+console.log("1"+45)
+console.log("1"+"1")
+console.log(1+4+"5")
