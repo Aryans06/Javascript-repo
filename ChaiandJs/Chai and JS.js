4a53bcd0bf9492322826fab9dcbb1c5746087038
Aryan <aryan@Aryans-MacBook-Air.local>
2024-12-31 09:55:44 +0530

    Added

---
+++ ChaiandJs/Chai and JS.js
@@ -0,0 +1,10 @@
+const accountId=14453
+let Email="aryans2637@gmail.com"
+var password="jhakas"
+accountCity="Jaipur"
+
+console.table([accountId,Email,password,accountCity])
+
+/*
+Dont use var because of isse in Block Scope and functional scope
+*/
---
+++ ChaiandJs/Datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all JS code as newer version
+// cant be used in nodejs alert(3+3)
+
+let name="Aryan"
+let age=20
+let isLogged=true
+/*
+Data types
+Number
+bigint
+string
+boolean
+null-standalone value
+undefined
+symbol-unique
+
+//object
+*/
+
+console.log(typeof null)//object
