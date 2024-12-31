533a0f3bd8bec8b03bb1d0e14503004acbf8208f
Aryan Jha <aryans2637@gmail.com>
2024-12-31 12:08:54 +0530

    Memory

--- ChaiandJs/datasummary.js
+++ ChaiandJs/datasummary.js
@@ -1,7 +1,7 @@
 // Primitve
 // 7types: string,Number,Bigint,symbol,null,boolean,undefined
 
-//reference(no primitive)
+//reference(non primitive)
 
 //Array,objects,functions
 
@@ -13,3 +13,24 @@
 const myFunction= function(){
   console.log("Hello")
 }
+
+//Stack(Primitve) and Heap Memory(Non primitive)
+let Myname="Aryan"
+let anothername= Myname
+anothername="Pallavi"
+
+console.log(Myname)
+console.log(anothername)
+
+
+let User={
+  Useremail:"aryans2637",
+  upi:"upi@ybl"
+
+}
+
+let Usertwo=User
+Usertwo.email="Yahoo@gmail.com"
+
+console.log(User.email)
+console.log(Usertwo.email)
