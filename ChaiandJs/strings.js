a6f3e9d16b473512e74ed8882e8581e07ff1a77a
Aryan Jha <aryans2637@gmail.com>
2024-12-31 14:55:30 +0530

    Strings Javscript

---
+++ ChaiandJs/strings.js
@@ -0,0 +1,10 @@
+const name="Aryan"
+ const leetcodeCount=105
+
+ console.log(`Hello my name is ${name} and my leetCode count is ${leetcodeCount}`);
+
+ const variable=new String('AryanJha')
+
+ console.log(variable.charAt(3))
+ console.log(variable.toLowerCase())
+ console.log(variable.startsWith())
