604c5b9aa107e5ee3c5388f4251ab3eddd38465c
Aryan Jha <aryans2637@gmail.com>
2025-01-01 10:32:47 +0530

    Number and Maths

---
+++ ChaiandJs/NumberAndMaths.js
@@ -0,0 +1,19 @@
+const score=435
+console.log(score)
+
+const balance=  new Number(200.748738)
+console.log(balance)
+
+//console.log(balance.toFixed(2))
+//console.log(balance.toString.length);
+//console.log(Math.floor(4.7))
+//console.log(Math.ceil(4.7))
+//console.log(Math.round(4.7))
+//console.log(Math.abs(-5))
+
+console.log(Math.random())
+console.log(Math.random()*5+1)
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
