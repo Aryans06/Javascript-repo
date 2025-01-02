1de5ac2d8f67857c1f13a091e5e7c3f3b8b35c55
Aryan Jha <aryans2637@gmail.com>
2025-01-02 13:57:39 +0530

    Arrow Function

---
+++ ChaiandJs/ArrowFunc.js
@@ -0,0 +1,27 @@
+const user={
+ username:"Aryan",
+ age:20,
+
+ welcomeMessage:function(){
+   console.log(`${this},welcome to website`)
+   console.log(this);
+}
+
+}
+//user.welcomeMessage()
+//user.username="Anupam"
+//user.welcomeMessage()
+
+//console.log(this)
+
+function chai(){
+ console.log(this)
+}
+
+chai()
+
+//const addTwo=(num1,num2)=>{
+ //  return num1+num2
+//}
+
+//const add=(num1,num2)=>(num1+num2)
