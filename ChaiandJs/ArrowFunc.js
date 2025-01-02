1642eb923f37a024ee48929a7c5b5827afbda3d4
Aryan Jha <aryans2637@gmail.com>
2025-01-02 14:01:03 +0530

    Arrow Functions

--- ChaiandJs/ArrowFunc.js
+++ ChaiandJs/ArrowFunc.js
@@ -18,10 +18,16 @@ function chai(){
  console.log(this)
 }
 
-chai()
+//chai()
 
 //const addTwo=(num1,num2)=>{
  //  return num1+num2
 //}
 
 //const add=(num1,num2)=>(num1+num2)
+
+// Immediately Invoked Function Expressions(IIFE)
+
+(function hello(){
+ console.log("connected")
+})()
